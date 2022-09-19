9ffc9f33038e08da057e30d270db1733558ae489
Mehe_od76 <Mehe_od76@STHQ0129-11>
2022-09-19 17:41:59 +0400

    Add project files.

---
+++ GarbageCollector/Program.cs
@@ -0,0 +1,44 @@
+﻿namespace GarbageCollector;
+
+class Student
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+
+    public Student()
+    {
+        Console.WriteLine("Constructor - Gen:");
+        Console.WriteLine(GC.GetGeneration(this));
+    }
+
+    ~Student() // Finalizer
+    {
+        Console.WriteLine($"Finalizer {Id}");
+    }
+}
+
+
+
+class Program
+{
+    static void DoSomething()
+    {
+        //Student s1 = new();
+        //Student s2 = new();
+
+        for (int i = 0; i < 100; i++)
+        {
+            new Student() { Id = i };
+        }
+    }
+
+
+    static void Main()
+    {
+        FileStream fs = new FileStream("myFile.txt",FileMode.Create); // 2
+        StreamWriter sw = new StreamWriter(fs); // 1
+        DoSomething();
+        GC.Collect();
+        GC.WaitForPendingFinalizers();
+}
+}
